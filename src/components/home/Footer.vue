5aa943af3a7712438a9dc9c614a6919f64d8d004
DPjjchen <417169837@qq.com>
2019-02-27 17:00:37 +0800

    add recommend

---
+++ src/components/home/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='footer'>
+    <span >Qunar 粤ICP备05XXXXXXX1087</span>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped lang='stylus'>
+  .footer
+    overflow hidden
+    height 0.5rem
+    margin: 0px;
+    padding: 0px;
+    color: #9e9e9e;
+    text-align: center;
+    font-size: 14px;
+
+</style>
